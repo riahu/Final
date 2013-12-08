91e52b4761c6259fae63638b66b62dae8f4db480
Kasey Alusi <kasey.alusi@gmail.com>
2013-12-08 15:08:08 -0800

    no message

--- js/cart.js
+++ js/cart.js
@@ -1,15 +1,36 @@
 
 
-var cart = [];
+var cart = {items: []};
 
 
 
 
-function add(name, id){
-	var cItem = [name, id];
+function add(namex, idx){
+	var cItem = {name: namex, id: idx};
 
-	cart.push(cItem);
+	cart.items.push(cItem);
 }
+
+function buildCart(){
+	var sidebar = $('.sidebar');
+	var ctemp = sidebar.find('.ctemp');
+
+    for (idx = 0; idx < cart.items.length; ++idx) {
+        item = cart.items[idx];
+        var clone = ctemp;
+        clone.find('.cIcon').css("background-image: url(img/" + item.id + ".png);");
+        clone.removeClass("ctemp");
+        sidebar.append(clone);
+    }}
+
+function testCart(){
+	add("Pika", 2);
+	add("Pika", 4);
+	add("Pika", 3);
+
+}
+
+
 
 $(function(){
 	//update the cart now? Or after each addition?
@@ -18,7 +39,8 @@ $(function(){
 		sidebar.toggle("fast");
 	});
 
-
+	testCart();
+	buildCart();
 
 
 });
