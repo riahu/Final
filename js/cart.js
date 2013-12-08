09a184f454326827f32c7a9579028f45ae697060
Kasey Alusi <kasey.alusi@gmail.com>
2013-12-08 12:52:26 -0800

    no message

--- js/cart.js
+++ js/cart.js
@@ -4,4 +4,15 @@ $(function(){
 		var sidebar = $('.sidebar');
 		sidebar.toggle("fast");
 	});
+
+
 });
+
+var cart = [];
+
+
+function add(string name, int id){
+	var cItem = [name, id];
+
+	cart.push(cItem);
+}
